3d195fd4feb74ac694d58f2b79bddb7f6ec00897
Ovi Fritsch <ovidiu.fritsch@gmail.com>
2018-08-23 16:22:05 -0700

    add association between user and articles and add debugger view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,9 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    
+    # hardcode
+    @article.user = User.first
     if @article.save
       flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
